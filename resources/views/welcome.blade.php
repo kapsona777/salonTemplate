defe9f0db89a49322dea2b70c568deb9dbb09705
kapsona777 <lasha.kapanadze.1@btu.edu.ge>
2023-12-19 16:40:02 +0400

    s

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>salonTemplate</title>
+        <link rel="icon" href="https://static5.depositphotos.com/1035649/531/v/950/depositphotos_5313891-stock-illustration-icon-beauty-salon.jpg">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
